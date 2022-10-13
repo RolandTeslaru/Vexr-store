051e7f9ad02bd1c034271f83601b16fb47d12e6b
Roland Teslaru <89258659+RolandTeslaru@users.noreply.github.com>
2022-10-14 01:24:43 +0300

    fixed GA

--- site/pages/_app.tsx
+++ site/pages/_app.tsx
@@ -46,22 +46,19 @@ export default function MyApp({ Component, pageProps }: AppProps) {
   }, [])
   return (
     <>
-      <Script
-        strategy='lazyOnload'
+    <Script
         src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        strategy="afterInteractive"
       />
-      <Script id='google-analytics' strategy='lazyOnload'>
+      <Script id="google-analytics" strategy="afterInteractive">
         {`
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){window.dataLayer.push(arguments);}
-           gtag('js', new Date());
-          
-           gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
-            page_path: window.location.pathname
-           });
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}');
         `}
       </Script>
-      <Head />
       <Loading/>
       <ManagedUIContext>
         <Layout pageProps={pageProps}>
